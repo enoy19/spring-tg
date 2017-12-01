817ed5542601c86be6afe8226a4474da24f61ce9
did10 <mathias1999@gmx.net>
2017-12-02 00:48:28 +0100

    Implement test for property injection in tgBot

--- src/test/java/io.enoy.tg/TgBotSetValueTest.java
+++ src/test/java/io.enoy.tg/TgBotSetValueTest.java
@@ -1,11 +1,48 @@
 package io.enoy.tg;
 
+import io.enoy.tg.bot.TgBot;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {TestConfiguration.class})
 public class TgBotSetValueTest
 {
+	@Autowired
+	private ApplicationContext context;
+	@Value("${bot.token}")
+	private String botToken;
+	@Value("${bot.name}")
+	private String botUsername;
+
+	private TgBot tgBot;
+
+	@Before
+	public void setUpTgBot()
+	{
+		assumeNotNull(context, botToken, botUsername);
+		tgBot = new TgBot(context);
+	}
+
+	@Test
+	public void shouldSetBotToken()
+	{
+		assertThat(tgBot.getBotToken(), is(botToken));
+	}
+
+	@Test
+	public void shouldSetBotUsername()
+	{
+		assertThat(tgBot.getBotUsername(), is(botUsername));
+	}
 }
