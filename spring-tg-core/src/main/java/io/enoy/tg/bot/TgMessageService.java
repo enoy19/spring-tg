e6fe3a0dbd2c74fa430e0e29fa419117eeb4ef8d
enoy <enoy19@gmail.com>
2018-01-25 21:11:49 +0100

    tg message service sends to chat now

--- spring-tg-core/src/main/java/io/enoy/tg/bot/TgMessageService.java
+++ spring-tg-core/src/main/java/io/enoy/tg/bot/TgMessageService.java
@@ -27,7 +27,7 @@ public class TgMessageService {
      * @see TgMessageServiceContextless#sendMessage(long, String)
      */
     public Message sendMessage(String message) {
-        return messageServiceContextless.sendMessage(getCurrentUserId(), message);
+        return messageServiceContextless.sendMessage(getCurrentChatId(), message);
     }
 
     /**
@@ -41,31 +41,31 @@ public class TgMessageService {
      * @see TgMessageServiceContextless#sendReplyKeyboard(long, String, List, boolean, int)
      */
     public Message sendReplyKeyboard(String message, List<String> keyboardData, boolean oneTimeKeyboard, int maxColumns) {
-        return messageServiceContextless.sendReplyKeyboard(getCurrentUserId(), message, keyboardData, oneTimeKeyboard, maxColumns);
+        return messageServiceContextless.sendReplyKeyboard(getCurrentChatId(), message, keyboardData, oneTimeKeyboard, maxColumns);
     }
 
     /**
      * @see TgMessageServiceContextless#sendReplyKeyboard(long, String, List, boolean)
      */
     public Message sendReplyKeyboard(String message, List<List<String>> keyboardText, boolean oneTimeKeyboard) {
-        return messageServiceContextless.sendReplyKeyboard(getCurrentUserId(), message, keyboardText, oneTimeKeyboard);
+        return messageServiceContextless.sendReplyKeyboard(getCurrentChatId(), message, keyboardText, oneTimeKeyboard);
     }
 
     /**
      * @see TgMessageServiceContextless#sendReplyKeyboard(long, String, String[][], boolean)
      */
     public Message sendReplyKeyboard(String message, String[][] keyboardText, boolean oneTimeKeyboard) {
-        return messageServiceContextless.sendReplyKeyboard(getCurrentUserId(), message, keyboardText, oneTimeKeyboard);
+        return messageServiceContextless.sendReplyKeyboard(getCurrentChatId(), message, keyboardText, oneTimeKeyboard);
     }
 
     /**
      * @see TgMessageServiceContextless#removeKeyboard(long)
      */
     public void removeKeyboard() {
-        messageServiceContextless.removeKeyboard(getCurrentUserId());
+        messageServiceContextless.removeKeyboard(getCurrentChatId());
     }
 
-    private long getCurrentUserId() {
+    private long getCurrentChatId() {
         return TgContextHolder.currentContext().getUserId();
     }
 
